28ea650164fdea5add03277258ade9e8575a3348
Busari Moruf <busarimoruf20@gmail.com>
2025-04-05 23:40:50 -0700

    new changes

---
+++ src/components/common/heading/Head.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+        <section className='head'>
+            <div className='container flexSB'>
+                <div className="logo">
+                    <h1>ACADEMIA</h1>
+                    <span>ONLINE EDUCATION & LEARNING</span>
+                </div>
+
+                <div className="social">
+                    <i className="fab fa-facebook-f"></i>
+                    <i className="fa fa-instagram"></i>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Head
