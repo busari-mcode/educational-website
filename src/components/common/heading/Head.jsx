931afbdb4f22a12db4e680ca7b30dc54cbeca1f4
Busari Moruf <busarimoruf20@gmail.com>
2025-04-06 00:44:28 -0700

    new changes

--- src/components/common/heading/Head.jsx
+++ src/components/common/heading/Head.jsx
@@ -11,8 +11,10 @@ const Head = () => {
                 </div>
 
                 <div className="social">
-                    <i className="fab fa-facebook-f"></i>
-                    <i className="fa fa-instagram"></i>
+                    <i className="fab fa-facebook-f icon"></i>
+                    <i className="fab fa-instagram icon"></i>
+                    <i className="fab fa-twitter icon"></i>
+                    <i className="fab fa-youtube icon"></i>
                 </div>
             </div>
         </section>
