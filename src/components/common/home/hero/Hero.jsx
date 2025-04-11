3aff1ba26bc2c2170343636e3f4cf7cb6d7c91da
Busari Moruf <busarimoruf20@gmail.com>
2025-04-10 19:46:53 -0700

    new changes

--- src/components/common/home/hero/Hero.jsx
+++ src/components/common/home/hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './hero.css'
+import Title from "../../title/Title"
 
 const Hero = () => {
   return (
@@ -7,12 +8,13 @@ const Hero = () => {
       <section className="hero">
         <div className="container">
             <div className="row">
+                <Title subtitle='WELCOME TO ACADEMIA' title='Best Online Education'/>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor hic totam, deleniti voluptates error officiis, necessitatibus beatae quis, iure laboriosam laudantium? Optio veniam doloribus repellat. Ducimus mollitia dignissimos quisquam debitis?</p>
                 <div className="button">
                     <button className="primary-btn">
                         GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i>
                     </button>
-                    <button className="primary-btn">
+                    <button>
                         VIEW COURSE <i className="fa fa-long-arrow-alt-right"></i>
                     </button>
                 </div>
