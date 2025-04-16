5a6ec7534b9877e05f85c2d4bc1e89622c7b81ea
Busari Moruf <busarimoruf20@gmail.com>
2025-04-15 22:18:50 -0700

    new changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/common/heading/Header';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/common/home/Home';
 import About from './components/about/About';
+import CourseHome from './components/allcourses/CourseHome';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" exact component={About} />
+          <Route path="/courses" exact component={CourseHome} />
         </Switch>
       </Router>
     </>
