02aab9c4a2f44dadd6524cf4de583b3dfd1d2abc
Busari Moruf <busarimoruf20@gmail.com>
2025-04-21 22:12:39 -0700

    new changes

--- src/components/allcourses/CoursesCard.jsx
+++ src/components/allcourses/CoursesCard.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { coursesCard } from '../../dummydata'
 
 const CoursesCard = () => {
   return (
     <>
-        <h1>CourseCard</h1>
+        <section className='coursesCard'>
+            <div className="container grid2">
+                {coursesCard.map((val) => {
+                    return(
+                    <div className="items">
+                        <div className="content flex">
+                            <div className="left">
+                                <div className="img">
+                                    <img src={val.cover} alt="" />
+                                </div>
+                            </div>
+                            <div className="text">
+                                <h1>{val.coursesName}</h1>
+                                <div className="rate">
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <i className="fa fa-star"></i>
+                                    <label htmlFor="">(5.0)</label>
+                                </div>
+                                <div className="details">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    )
+                })}
+            </div>
+        </section>
     </>
   )
 }
